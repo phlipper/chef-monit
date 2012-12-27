c66db22e6e2d8f693e47f344601f6d8359cb7024
Phil Cohen <github@phlippers.net>
2012-12-26 22:48:04 -0800

    FC017: LWRP does not notify when updated

--- providers/monitrc.rb
+++ providers/monitrc.rb
@@ -8,10 +8,15 @@ action :create do
     notifies :restart, "service[monit]", :immediately
     action :create
   end
+
+  # My state has changed so I'd better notify observers
+  new_resource.updated_by_last_action(true)
 end
 
 action :delete do
   execute "delete monitrc" do
     command "rm -f #{node["monit"]["includes_dir"]}/#{new_resource.name}.monitrc"
   end
+
+  new_resource.updated_by_last_action(true)
 end
