b75040755a43620fd6491fbcd74483523a164b6d
Thomas Dudziak <tomdzk@gmail.com>
2013-03-14 12:48:55 -0700

    Support templates in other cookbooks, and use the file resource to delete the config file

--- resources/monitrc.rb
+++ resources/monitrc.rb
@@ -3,3 +3,7 @@ default_action :create
 
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :variables, :kind_of => Hash
+# the cookbook containing the template, defaults to the one invoking this LWRP
+attribute :template_source, :kind_of => String
+# the template file name, defaults to #{name}.monitrc.erb
+attribute :template_cookbook, :kind_of => String
