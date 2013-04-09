72425a337f83fd06d0a7c61708297907627d6940
Phil Cohen <github@phlippers.net>
2013-04-09 10:25:42 -0700

    bump version to 1.3.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "github@phlippers.net"
 license           "MIT"
 description       "Configures monit"
 long_description  "Please refer to README.md"
-version           "1.3.0"
+version           "1.3.1"
 
 recipe "monit", "Sets up the service definition and default checks."
 
