ccaae819e159b6081eb943bda7196e94c21ef183
Christopher H. Laco <claco@chrislaco.com>
2013-06-14 23:34:19 -0400

    Only restart if the template output has actually changed.

--- providers/monitrc.rb
+++ providers/monitrc.rb
@@ -1,23 +1,23 @@
 action :create do
-  template "#{node["monit"]["includes_dir"]}/#{new_resource.name}.monitrc" do
+  t = template "#{node["monit"]["includes_dir"]}/#{new_resource.name}.monitrc" do
     owner "root"
     group "root"
     mode  "0644"
     source new_resource.template_source || "#{new_resource.name}.monitrc.erb"
     cookbook new_resource.template_cookbook
     variables new_resource.variables
-    notifies :restart, "service[monit]", :immediately
     action :create
+    notifies :restart, "service[monit]", :delayed
   end
 
-  # My state has changed so I'd better notify observers
-  new_resource.updated_by_last_action(true)
+  new_resource.updated_by_last_action(t.updated_by_last_action?)
 end
 
 action :delete do
-  file "#{node["monit"]["includes_dir"]}/#{new_resource.name}.monitrc" do
+  f = file "#{node["monit"]["includes_dir"]}/#{new_resource.name}.monitrc" do
     action :delete
+    notifies :restart, "service[monit]", :delayed
   end
 
-  new_resource.updated_by_last_action(true)
+  new_resource.updated_by_last_action(f.updated_by_last_action?)
 end
