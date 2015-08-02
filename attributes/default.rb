c39c60f31cc00ae88c5fb762b2f003a5e1e46cc0
Jim Myhrberg <contact@jimeh.me>
2015-08-02 10:07:54 +0100

    Add support for custom include paths
    For example using Dokku and it's monit plugin, I need to specifically
    include "/home/dokku/*/monitrc" which wasn't possible previously.

--- attributes/default.rb
+++ attributes/default.rb
@@ -71,6 +71,8 @@ else
   default["monit"]["includes_dir"] = "/etc/monit/conf.d"
 end
 
+default["monit"]["includes"] = []
+
 # The monit::default recipe will load these monit_monitrc resources automatically
 # NOTE setting this attribute at the default level will append values to the array
 default["monit"]["default_monitrc_configs"] = %w[load ssh]
